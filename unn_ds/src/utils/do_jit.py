cb48a44699df57309e216f8bc6b4d2239ef611d4
DimaAmega <dima.amega1@gmail.com>
2022-04-12 02:40:30 +0300

    added cache

--- unn_ds/src/utils/do_jit.py
+++ unn_ds/src/utils/do_jit.py
@@ -1,5 +1,7 @@
 from numba import njit
 from numba.core.extending import is_jitted
+from functools import lru_cache
 
+@lru_cache(maxsize=None)
 def do_jit(F):
     return njit(F) if not is_jitted(F) else F
