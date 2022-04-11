cd23a030322943614c7314ba6d73ea04cec7288a
DimaAmega <dima.amega1@gmail.com>
2022-04-12 02:42:16 +0300

    added jit_newton_without_yacoby

--- unn_ds/src/optimizers/jit_newton.py
+++ unn_ds/src/optimizers/jit_newton.py
@@ -3,7 +3,7 @@ from numpy.linalg import inv, norm
 import numpy as np
 
 @njit
-def jit_yacoby_matrix_numeric(VF, X, h=1e-3):
+def __jit_yacoby_matrix_numeric(VF, X, h=1e-3):
     N = len(X)
     y_m = np.empty((N, N))
     X_1, X_2 = X.copy(), X.copy()
@@ -15,6 +15,23 @@ def jit_yacoby_matrix_numeric(VF, X, h=1e-3):
         X_1[i] += h
         X_2[i] -= h
     return y_m
+
+@njit
+def jit_newton_without_yacoby(VF, X0, eps, K_MAX, verify_x):
+    X = X0
+    Y_1 = inv(__jit_yacoby_matrix_numeric(VF, X))
+    d = Y_1 @ VF(X)
+    iteration = 0
+    while norm(d) > eps and iteration < K_MAX:
+        if not verify_x(X):
+            raise ArithmeticError("We can't find the point")
+        iteration += 1
+        Y_1 = inv(__jit_yacoby_matrix_numeric(VF, X))
+        d = Y_1 @ VF(X)
+        X -= d
+    if iteration == K_MAX:
+        raise ArithmeticError("Too much iterations")
+    return X
 
 @njit
 def jit_newton(VF, X0, eps, K_MAX, jit_yacoby_matrix, verify_x):
--- unn_ds/src/optimizers/newton.py
+++ unn_ds/src/optimizers/newton.py
@@ -1,15 +1,10 @@
-from . jit_newton import jit_newton as __jit_newton
-from . jit_newton import jit_yacoby_matrix_numeric as __jit_yacoby_matrix_numeric
+from . jit_newton import jit_newton as __jit_newton, jit_newton_without_yacoby as __jit_newton_without_yacoby
 from .. utils.do_jit import do_jit as __do_jit
 
 def newton(VF, X0, eps=1e-3, K_MAX=100, yacoby_matrix = False, verify_x=lambda _: True):
     '''Explanation here https://www.wikiwand.com/en/Newton%27s_method'''
     
     if not yacoby_matrix:
-        jit_fv = __do_jit(VF)
-        def wrap(X):
-            return __jit_yacoby_matrix_numeric(jit_fv, X)
-
-        yacoby_matrix = wrap
+        return __jit_newton_without_yacoby(__do_jit(VF), X0, eps, K_MAX, __do_jit(verify_x))
 
     return __jit_newton(__do_jit(VF), X0, eps, K_MAX, __do_jit(yacoby_matrix), __do_jit(verify_x))
