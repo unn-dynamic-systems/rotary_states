b1cd3757dbeb4775a08332b487613f3051364a20
DimaAmega <dima.amega1@gmail.com>
2022-04-12 02:43:03 +0300

    dry

--- tests/test_newton.py
+++ tests/test_newton.py
@@ -2,27 +2,27 @@ import numpy as np
 
 from unn_ds import optimizers
 
+def VF1(X):
+    return np.array([2 * X[0] ** 2 + X[1] ** 2 - 1, X[0] ** 3 + 6 * X[0] ** 2 * X[1] - 1])
+
+def VF2(X):
+    return np.array([2 * X[0] ** 2 + X[1] ** 2 - 1, X[0] ** 3 + 6 * X[0] ** 2 * X[1]])
+
 def compare_vectors(v1, v2, eps=1e-5):
     norm = np.linalg.norm(v1 - v2)
     assert norm < eps
 
 def test_newton1():
-    def VF(X):
-        return np.array([2 * X[0] ** 2 + X[1] ** 2 - 1, X[0] ** 3 + 6 * X[0] ** 2 * X[1] - 1])
-
     X0 = np.array([0.9, 0.18])
     print(f"newton started from {X0}")
-    X = optimizers.newton(VF, X0)
+    X = optimizers.newton(VF1, X0)
     answer = np.array([0.68659442, 0.23911545])
     compare_vectors(answer, X)
-    compare_vectors(VF(X), np.array([0, 0]))
+    compare_vectors(VF1(X), np.array([0, 0]))
     print(f"we got {X}, well done")
 
 
 def test_newton1_yacoby():
-
-    def VF(X):
-        return np.array([2 * X[0] ** 2 + X[1] ** 2 - 1, X[0] ** 3 + 6 * X[0] ** 2 * X[1] - 1])
     
     def yacoby_matrix(X):
         return np.array([[4 * X[0], 2 * X[1]],
@@ -30,23 +30,20 @@ def test_newton1_yacoby():
 
     X0 = np.array([0.9, 0.18])
     print(f"newton started from {X0}")
-    X = optimizers.newton(VF, X0, yacoby_matrix=yacoby_matrix)
+    X = optimizers.newton(VF1, X0, yacoby_matrix=yacoby_matrix)
     answer = np.array([0.68659442, 0.23911545])
     compare_vectors(answer, X)
-    compare_vectors(VF(X), np.array([0, 0]))
+    compare_vectors(VF1(X), np.array([0, 0]))
     print(f"we got {X}, well done")
 
 def test_newton3():
 
-    def VF(X):
-        return np.array([2 * X[0] ** 2 + X[1] ** 2 - 1, X[0] ** 3 + 6 * X[0] ** 2 * X[1]])
-
     X0 = np.array([0.9, 0.18])
     print(f"newton started from {X0}")
-    X = optimizers.newton(VF, X0, eps=1e-5)
+    X = optimizers.newton(VF2, X0, eps=1e-5)
     answer = np.array([0.70224689, -0.11704113])
     compare_vectors(answer, X)
-    compare_vectors(VF(X), np.array([0, 0]))
+    compare_vectors(VF2(X), np.array([0, 0]))
     print(f"we got {X}, well done")
 
 
